ab712ba8a4b452e479ba3d501944c51e9ae08b6a
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-11-29 17:05:32 -0600

    Implement LLVM FunctionValue building.

--- src/llvm/ir.rs
+++ src/llvm/ir.rs
@@ -3,7 +3,8 @@ use std::sync::LazyLock;
 use crate::ast::*;
 use crate::symbol::*;
 
-use inkwell::types::BasicType;
+use inkwell::types::{BasicMetadataTypeEnum, BasicType};
+use inkwell::values::FunctionValue;
 use inkwell::AddressSpace;
 use inkwell::{
     builder::Builder,
@@ -17,6 +18,8 @@ pub struct LlvmGenVisitor<'a, 'ctx> {
     builder: &'a Builder<'ctx>,
 }
 
+/// Constant for readability. ADDRESS_SPACE_GLOBAL is defined as 1 in LLVM's
+/// AddressSpace enum.
 static ADDRESS_SPACE_GLOBAL: LazyLock<AddressSpace> = LazyLock::new(|| {
     AddressSpace::from(1u16)
 });
@@ -72,11 +75,56 @@ impl<'a, 'ctx> LlvmGenVisitor<'a, 'ctx> {
         }
     }
 
-    fn visit_decl_fn(&self, fun: &decl::Function<'_>) {
-        let Some(body) = &fun.body else { return; };
+    fn visit_decl_fn(&self, r#fn: &decl::Function<'_>) {
+        let llvm_fn = match self.module.get_function(r#fn.name) {
+            None => self.gen_fn_prototype(r#fn),
+            Some(llvm_fn) => llvm_fn,
+        };
 
-        todo!();
+        if let Some(body) = &r#fn.body {
+            let block = self.context.append_basic_block(
+                llvm_fn,
+                &format!("{}_enter", r#fn.name)
+            );
+            self.builder.position_at_end(block);
+            self.visit_stmt(body);
+        }
     }
+
+    fn gen_fn_prototype(&self, r#fn: &decl::Function<'_>) -> FunctionValue<'ctx> {
+        let Type::Function(fn_type) = &*r#fn.r#type else { unreachable!() };
+
+        let llvm_fn_type = {
+                let param_types = fn_type.params.iter()
+                    .map(|param| {
+                        (*generate_basic_type(&self.context, &param.r#type))
+                            .as_basic_type_enum()
+                            .into()
+                    })
+                    .collect::<Vec<BasicMetadataTypeEnum>>();
+
+                if let Type::Atomic(Atomic::Void, ..) = &*fn_type.return_type {
+                    self.context.void_type().fn_type(&param_types, false)
+                } else {
+                    generate_basic_type(&self.context, &*fn_type.return_type)
+                        .fn_type(&param_types, false)
+                }
+        };
+
+        let llvm_fn = self.module.add_function(
+            &r#fn.name,
+            llvm_fn_type,
+            None
+        );
+
+        llvm_fn.get_param_iter()
+            .zip(&fn_type.params)
+            .for_each(|(llvm_param, param)| llvm_param.set_name(&param.ident));
+
+        llvm_fn
+    }
+
+    fn visit_stmt(&self, stmt: &Stmt<'_>) {}
 }
 
 fn generate_basic_type<'ctx>(
@@ -98,6 +146,6 @@ fn generate_basic_type<'ctx>(
             ),
             r#type::ArraySize::Unknown => todo!(),
         },
-        Type::Function(..) => unreachable!("variable can't have type Function"),
+        Type::Function(..) => unreachable!("variable can't be type Function"),
     }
 }
