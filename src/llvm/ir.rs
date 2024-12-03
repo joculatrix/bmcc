04b3ecae6df015c7d14c330628a5b800be5651b5
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-12-02 22:01:20 -0600

    Implement codegen for control flow statements.

--- src/llvm/ir.rs
+++ src/llvm/ir.rs
@@ -1,26 +1,30 @@
+use std::cell::RefCell;
 use std::sync::LazyLock;
 
 use crate::ast::*;
 use crate::symbol::*;
 
-use inkwell::types::{BasicMetadataTypeEnum, BasicType};
-use inkwell::values::FunctionValue;
-use inkwell::AddressSpace;
+use inkwell::values::AnyValueEnum;
+use inkwell::IntPredicate;
 use inkwell::{
     builder::Builder,
     context::Context,
     module::Module,
+    types::{ BasicMetadataTypeEnum, BasicType },
+    values::FunctionValue,
+    AddressSpace,
 };
 
 pub struct LlvmGenVisitor<'a, 'ctx> {
     context: &'ctx Context,
     module: &'a Module<'ctx>,
     builder: &'a Builder<'ctx>,
+    curr_fn: RefCell<Option<FunctionValue<'ctx>>>,
 }
 
 /// Constant for readability. ADDRESS_SPACE_GLOBAL is defined as 1 in LLVM's
 /// AddressSpace enum.
-static ADDRESS_SPACE_GLOBAL: LazyLock<AddressSpace> = LazyLock::new(|| {
+const ADDRESS_SPACE_GLOBAL: LazyLock<AddressSpace> = LazyLock::new(|| {
     AddressSpace::from(1u16)
 });
 
@@ -37,9 +41,9 @@ impl<'a, 'ctx> LlvmGenVisitor<'a, 'ctx> {
     fn new(
         context: &'ctx Context,
         module: &'a Module<'ctx>,
-        builder: &'a Builder<'ctx>
+        builder: &'a Builder<'ctx>,
     ) -> LlvmGenVisitor<'a, 'ctx> {
-        LlvmGenVisitor { context, module, builder }
+        LlvmGenVisitor { context, module, builder, curr_fn: RefCell::new(None) }
     }
 
     fn run(&self, ast: &Vec<Decl<'_>>) -> Result<(), Vec<()>> {
@@ -87,7 +91,13 @@ impl<'a, 'ctx> LlvmGenVisitor<'a, 'ctx> {
                 &format!("{}_enter", r#fn.name)
             );
             self.builder.position_at_end(block);
+
+            let mut curr_fn = self.curr_fn.borrow_mut();
+            *curr_fn = Some(llvm_fn);
+
             self.visit_stmt(body);
+
+            *curr_fn = None;
         }
     }
 
@@ -124,7 +134,134 @@ impl<'a, 'ctx> LlvmGenVisitor<'a, 'ctx> {
         llvm_fn
     }
 
-    fn visit_stmt(&self, stmt: &Stmt<'_>) {}
+    fn visit_expr(&self, expr: &Expr<'_>) -> AnyValueEnum<'ctx> {}
+
+    fn visit_stmt(&self, stmt: &Stmt<'_>) {
+        match stmt {
+            Stmt::Block(stmts, ..) => for stmt in stmts { self.visit_stmt(stmt) },
+            Stmt::Decl(decl, ..) => {
+                let Decl::Var(var) = &**decl else { unreachable!() };
+                self.visit_decl_var(var);
+            }
+            Stmt::Expr(expr, ..) => { self.visit_expr(expr); }
+            Stmt::Print(vec, ..) => todo!(),
+            Stmt::Return(expr, ..) => todo!(),
+            Stmt::If(if_stmt) => { self.visit_stmt_if(if_stmt); }
+            Stmt::For(for_stmt) => { self.visit_stmt_for(for_stmt); }
+            Stmt::While(while_stmt) => { self.visit_stmt_while(while_stmt); }
+        }
+    }
+
+    fn visit_stmt_if(&self, if_stmt: &stmt::IfStmt<'_>) {
+        let condition = match self.visit_expr(&*if_stmt.condition) {
+            AnyValueEnum::IntValue(val) => val,
+            _ => panic!("Condition expression should evaluate to bool/int"),
+        };
+
+        let cmp = self.builder
+            .build_int_compare(
+                IntPredicate::NE,
+                condition,
+                self.context.i64_type().const_zero(),
+                "ifcond",
+            )
+            .unwrap();
+        
+        let curr_fn = self.curr_fn.borrow().expect("Stmt can't exist outside function");
+        
+        let then = self.context.append_basic_block(curr_fn, "then");
+        let r#else = match &if_stmt.else_body {
+            Some(_) => Some(self.context.append_basic_block(curr_fn, "else")),
+            None => None,
+        };
+        let cont = self.context.append_basic_block(curr_fn, "cont");
+
+        self.builder.build_conditional_branch(
+            cmp,
+            then,
+            if let Some(r#else) = r#else { r#else } else { cont },
+        );
+
+        self.builder.position_at_end(then);
+        self.visit_stmt(&*if_stmt.body);
+        self.builder.build_unconditional_branch(cont);
+
+        if let Some(else_body) = &if_stmt.else_body {
+            let r#else = r#else.expect("Else block should exist when else body exists");
+            self.builder.position_at_end(r#else);
+            self.visit_stmt(&**else_body);
+            self.builder.build_unconditional_branch(cont);
+        }
+
+        self.builder.position_at_end(cont);
+    }
+
+    fn visit_stmt_for(&self, for_stmt: &stmt::ForStmt<'_>) {
+        let curr_fn = self.curr_fn.borrow().expect("Stmt can't exist outside function");
+
+        let loop_header = self.context.append_basic_block(curr_fn, "loop");
+        let loop_body = self.context.append_basic_block(curr_fn, "body");
+        let cont = self.context.append_basic_block(curr_fn, "cont");
+       
+        self.visit_expr(&*for_stmt.init_expr);
+        // ensure previous block ends with a branch:
+        self.builder.build_unconditional_branch(loop_header);
+
+        self.builder.position_at_end(loop_header);
+        self.visit_expr(&*for_stmt.init_expr);
+        let condition = match self.visit_expr(&*for_stmt.condition) {
+            AnyValueEnum::IntValue(val) => val,
+            _ => panic!("Condition expression should evaluate to bool/int"),
+        };
+        let cmp = self.builder
+            .build_int_compare(
+                IntPredicate::NE,
+                condition,
+                self.context.i64_type().const_zero(),
+                "lpcond",
+            )
+            .unwrap();
+        self.builder.build_conditional_branch(cmp, loop_body, cont);
+
+        self.builder.position_at_end(loop_body);
+        self.visit_stmt(&*for_stmt.body);
+        self.visit_expr(&*for_stmt.next_expr);
+        self.builder.build_unconditional_branch(loop_header);
+
+        self.builder.position_at_end(cont);
+    }
+
+    fn visit_stmt_while(&self, while_stmt: &stmt::WhileStmt<'_>) {
+        let curr_fn = self.curr_fn.borrow().expect("Stmt can't exist outside function");
+
+        let loop_header = self.context.append_basic_block(curr_fn, "loop");
+        let loop_body = self.context.append_basic_block(curr_fn, "body");
+        let cont = self.context.append_basic_block(curr_fn, "cont");
+
+        // ensure previous block ends with a branch:
+        self.builder.build_unconditional_branch(loop_header);
+        
+        self.builder.position_at_end(loop_header);
+        let condition = match self.visit_expr(&*while_stmt.condition) {
+            AnyValueEnum::IntValue(val) => val,
+            _ => panic!("Condition expression should evaluate to bool/int"),
+        };
+        let cmp = self.builder
+            .build_int_compare(
+                IntPredicate::NE,
+                condition,
+                self.context.i64_type().const_zero(),
+                "lpcond",
+            )
+            .unwrap();
+        self.builder.build_conditional_branch(cmp, loop_body, cont);
+
+        self.builder.position_at_end(loop_body);
+        self.visit_stmt(&*while_stmt.body);
+        self.builder.build_unconditional_branch(loop_header);
+
+        self.builder.position_at_end(cont);
+    }
 }
 
 fn generate_basic_type<'ctx>(
